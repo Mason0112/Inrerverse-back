c81c34360fed498d5fbf3622aaa81f9b6842356a
Nicole <pinhsiuwang@gmail.com>
2024-08-13 11:48:09 +0800

    first commit

---
+++ src/main/java/com/interverse/demo/model/User.java
@@ -0,0 +1,76 @@
+package com.interverse.demo.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity @Table(name="users")
+public class User {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(unique = true, nullable = false)
+	private String accountNumber;
+	@Column(nullable = false)
+	private String password;
+	@Column(unique = true, nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private String nickname;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss EEEE")
+	private LocalDateTime added;
+	
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
+	private UserDetail userDetail;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "sender")
+	private List<Notification> sentNotification;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "receiver")
+	private List<Notification> receivedNotification;
+	
+	
+	//暐欣的
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "users")
+	private List<Order> orders;
+	
+	
+	//靖緯的
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "creator")
+	private List<Club> club;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "creator")
+	private List<Event> event;
+	
+	
+	//勁甫的
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
+	private List<UserPost> userPost;
+	
+	@PrePersist
+    public void onCreate() {
+        if (added == null) {
+            added = LocalDateTime.now(); // 設置當前的日期時間
+        }
+    }
+}
---
+++ src/main/java/com/interverse/demo/service/UserService.java
@@ -0,0 +1,8 @@
+package com.interverse.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
