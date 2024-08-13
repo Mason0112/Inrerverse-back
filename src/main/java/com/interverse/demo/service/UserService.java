7ec7ada81020ae88348057d41123d39d870c0426
Nicole <pinhsiuwang@gmail.com>
2024-08-13 18:55:17 +0800

    JWT初始

--- src/main/java/com/interverse/demo/service/UserService.java
+++ src/main/java/com/interverse/demo/service/UserService.java
@@ -1,8 +1,16 @@
 package com.interverse.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.interverse.demo.model.UserRepository;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	
 
 }
---
+++ src/main/java/com/interverse/demo/util/JwtUtil.java
@@ -0,0 +1,15 @@
+package com.interverse.demo.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+	
+	@Value("${jwt.secret}")
+	private String secret;
+	
+	@Value("${jwt.token.expiration}")
+	private long expiration;
+
+}
