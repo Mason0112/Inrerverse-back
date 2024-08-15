2cb714cfe472a667c715ec17e4638d2bd8731f90
allen <allen@gmail.com.tw>
2024-08-15 16:46:14 +0800

    clubController新增方法成功，關掉ddl

--- src/main/java/com/interverse/demo/controller/ClubController.java
+++ src/main/java/com/interverse/demo/controller/ClubController.java
@@ -1,11 +1,14 @@
 package com.interverse.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.interverse.demo.model.Club;
 import com.interverse.demo.service.ClubService;
-import com.interverse.demo.service.UserService;
 
 
 
@@ -16,10 +19,11 @@ public class ClubController {
 	@Autowired
 	private ClubService cService;
 
-	@Autowired
-	private UserService uService;
-
-//	public String addClub(Model model) {
-//	
-//}
+//	@Autowired
+//	private UserService uService;
+	
+	@PostMapping
+	public Club createClub(@RequestBody Club club) {
+		return cService.saveClub(club);
+}
 }
