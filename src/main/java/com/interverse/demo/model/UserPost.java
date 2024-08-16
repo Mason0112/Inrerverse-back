b251911a5f8a6334179365bcece67ecb2cfb17b6
Lance <en7551629@gmail.com>
2024-08-16 09:58:52 +0800

    no message

--- src/main/java/com/interverse/demo/model/UserPost.java
+++ src/main/java/com/interverse/demo/model/UserPost.java
@@ -17,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
