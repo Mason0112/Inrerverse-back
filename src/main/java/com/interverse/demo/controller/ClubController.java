7bdfe7a50628f663c8f22817a09d1153b07be924
allen <allen@gmail.com.tw>
2024-08-16 16:34:31 +0800

    club刪除功能完成

--- src/main/java/com/interverse/demo/controller/ClubController.java
+++ src/main/java/com/interverse/demo/controller/ClubController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +48,17 @@ public class ClubController {
 	    	 
 	     }
 	     return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteClub(@PathVariable Integer id){		
+		
+		if(cService.findClubById(id)==null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("無此 ID");
+		}
+		
+		cService.deleteById(id);
+		
+        return ResponseEntity.status(HttpStatus.OK).body("Delete Successful");
 	}
 }
