e61d9b09f9f0dcf6688bb3cf0fe112cfedb97e45
Lance <en7551629@gmail.com>
2024-08-21 09:17:59 +0800

    0821 commit

--- src/main/java/com/interverse/demo/model/UserPost.java
+++ src/main/java/com/interverse/demo/model/UserPost.java
@@ -46,7 +46,8 @@ public class UserPost {
 	private List<PostComment> comment;
 	
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "userPost")
-	private List<PostPhotos> photos;
+	@JsonManagedReference("post-photos")
+	private List<PostPhoto> photos;
 	
 	@PrePersist // 當物件要進入persistent狀態前，先執行以下方法
 	public void onCreate() {
