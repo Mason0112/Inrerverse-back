fb2812070e5fab888ba8a22cbd4abaffefd61481
lance1014-d <en7551629@gmail.com>
2024-09-03 01:15:46 +0800

    小小修改按讚

--- src/main/java/com/interverse/demo/service/PostLikeService.java
+++ src/main/java/com/interverse/demo/service/PostLikeService.java
@@ -1,5 +1,7 @@
 package com.interverse.demo.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,8 @@ import com.interverse.demo.model.PostLike;
 import com.interverse.demo.model.PostLikeRepository;
 import com.interverse.demo.model.User;
 import com.interverse.demo.model.UserPost;
+import com.interverse.demo.model.UserPostRepository;
+import com.interverse.demo.model.UserRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -15,15 +19,26 @@ public class PostLikeService {
 
     @Autowired
     private PostLikeRepository postLikeRepository;
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+    private UserPostRepository userPostRepository;
 
     @Transactional
     public void toggleLike(Integer userId, Integer postId, Integer type) {
         if (postLikeRepository.existsByUserIdAndPostId(userId, postId)) {
             postLikeRepository.deleteByUserIdAndPostId(userId, postId);
         } else {
+        	Optional<User> userOptional = userRepository.findById(userId);
+        	Optional<UserPost> postOptional = userPostRepository.findById(postId);
+        	if(userOptional.isPresent()&& postOptional.isPresent()) {
+
+        	}
             PostLike postLike = new PostLike();
-            postLike.setUser(new User(userId));
-            postLike.setPost(new UserPost(postId));
+            postLike.setUser(userOptional.get());
+            postLike.setPost(postOptional.get());
             postLike.setType(type);
             postLikeRepository.save(postLike);
         }
