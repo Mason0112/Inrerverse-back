94aad9ab86dd790934edad5d1c1dfc8e6b4e342f
Mason <willy55406@gmail.com>
2024-09-04 17:02:08 +0800

    LinePay結束

--- src/main/java/com/interverse/demo/service/LinePayService.java
+++ src/main/java/com/interverse/demo/service/LinePayService.java
@@ -1,26 +1,38 @@
 package com.interverse.demo.service;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.HmacAlgorithms;
 import org.apache.commons.codec.digest.HmacUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.interverse.demo.dto.CartResponseDTO;
 import com.interverse.demo.dto.LinePayDTO;
 import com.interverse.demo.dto.OrderDTO;
+import com.interverse.demo.dto.OrderDetailDTO;
+import com.interverse.demo.model.Product;
+import com.interverse.demo.model.ProductRepository;
 import com.interverse.demo.model.linepay.CheckoutPaymentRequestForm;
 import com.interverse.demo.model.linepay.ProductForm;
 import com.interverse.demo.model.linepay.ProductPackageForm;
 import com.interverse.demo.model.linepay.RedirectUrls;
+import com.interverse.demo.util.PostApiUtil;
 
 
 @Service
 public class LinePayService{
+	
+	@Autowired
+	private PostApiUtil postApiUtil;
+	@Autowired
+	private ProductRepository pRepository;
 	
 	
 	public static String encrypt(final String keys, final String data) {
@@ -33,28 +45,48 @@ public class LinePayService{
     }
 	
 	
-	public LinePayDTO LinePayPost(OrderDTO dto) {
+	public JsonNode LinePayPost(OrderDTO dto) {
 		
 		CheckoutPaymentRequestForm form = new CheckoutPaymentRequestForm();
-		
-
-		form.setAmount(100);
+	
+		form.setAmount(dto.getTotalAmount());
 		form.setCurrency("TWD");
-		form.setOrderId("merchant_order_id");
+		form.setOrderId(dto.getId().toString());
 		
 		ProductPackageForm productPackageForm = new ProductPackageForm();
-		productPackageForm.setId("package_id");
-		productPackageForm.setName("shop_name");
-		productPackageForm.setAmount(100);
 		
-		ProductForm productForm = new ProductForm();
-		productForm.setId("product_id");
-		productForm.setName("product_name");
-		productForm.setImageUrl("");
-		productForm.setQuantity(10);
-		productForm.setPrice(10);
+		productPackageForm.setId(dto.getId().toString());
+		productPackageForm.setName("Interverse");
+		productPackageForm.setAmount(dto.getTotalAmount());
+		
+		
 		ArrayList<ProductForm> ProductFormList = new ArrayList<>();
-		ProductFormList.add(productForm);
+		
+		List<OrderDetailDTO> productForms = dto.getOrderDetails();
+		
+		for (OrderDetailDTO orderDetailDTO : productForms) {
+			
+			
+			System.out.println("總金額"+dto.getTotalAmount());
+			
+			
+			ProductForm productForm = new ProductForm();
+			Optional<Product> byId = pRepository.findById(orderDetailDTO.getProductId());
+			Product product = byId.get();
+			productForm.setId(product.getId().toString());
+			productForm.setName(product.getName());
+			productForm.setImageUrl("");
+			
+			System.out.println("數量"+orderDetailDTO.getQuantity());
+			System.out.println("金額"+orderDetailDTO.getPrice());
+			productForm.setQuantity(orderDetailDTO.getQuantity());
+			productForm.setPrice(orderDetailDTO.getPrice());
+			
+			ProductFormList.add(productForm);
+		}
+		
+		
+		
 		productPackageForm.setProducts(ProductFormList);
 		
 		
@@ -63,8 +95,8 @@ public class LinePayService{
 		ProductPackageFormList.add(productPackageForm);
 		form.setPackages(ProductPackageFormList);
 		RedirectUrls redirectUrls = new RedirectUrls();
-		redirectUrls.setAppPackageName("");
-		redirectUrls.setConfirmUrl("https://www.google.com");
+		redirectUrls.setAppPackageName("123");
+		redirectUrls.setConfirmUrl("http://localhost:5173/order/userOrders");
 		redirectUrls.setCancelUrl("");
 		form.setRedirectUrls(redirectUrls);
 		
@@ -75,7 +107,7 @@ public class LinePayService{
 		String channelId = "2005966585";
 		String channelSecret = "6ae54e020168ccb8f6af8f34e21e2efe";
 		String requestUri = "/v3/payments/request";
-		String requestHttpUri = "https://sandbox-api-line.me/v3/payments/request";
+		String requestHttpUri = "https://sandbox-api-pay.line.me/v3/payments/request";
 		String nonce = UUID.randomUUID().toString();
 		try {
 			System.out.println("body=>" +mapper.writeValueAsString(form));
@@ -90,7 +122,14 @@ public class LinePayService{
 			linePayDTO.setSignature(signature);
 			linePayDTO.setBody(mapper.writeValueAsString(form));
 			linePayDTO.setRequestHttpUri(requestHttpUri);
-			return linePayDTO;
+
+			
+			
+			 JsonNode sendPost = postApiUtil.sendPost(linePayDTO);
+			 
+			 System.out.println("sendPost" +sendPost);
+			 
+			 return sendPost;
 			
 		} catch (JsonProcessingException e) {
 			// TODO Auto-generated catch block
