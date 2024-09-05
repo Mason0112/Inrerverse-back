8ff50ae284235baf711da28ba4116dd73b863f09
Nicole <pinhsiuwang@gmail.com>
2024-09-05 13:25:11 +0800

    通知排序由新到舊

--- src/main/java/com/interverse/demo/model/NotificationRepository.java
+++ src/main/java/com/interverse/demo/model/NotificationRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
 	
-	@Query("from Notification where receiver.id = :id")
+	@Query("from Notification where receiver.id = :id ORDER BY added DESC")
 	List<Notification> findByReceiverId(@Param("id") Integer userId);
 	
 	@Query("SELECT COUNT(n) FROM Notification n WHERE n.receiver.id = :id AND n.status = false")
