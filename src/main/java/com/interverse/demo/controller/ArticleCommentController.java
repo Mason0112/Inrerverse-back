303604bb3e5b2a7580ee8c43ffcaa0d1a9d95e21
lance1014-d <en7551629@gmail.com>
2024-09-09 17:29:25 +0800

    分享文章

--- src/main/java/com/interverse/demo/controller/ArticleCommentController.java
+++ src/main/java/com/interverse/demo/controller/ArticleCommentController.java
@@ -5,17 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.interverse.demo.dto.ArticleCommentDTO;
-import com.interverse.demo.model.ClubArticleCommentRepository;
 import com.interverse.demo.service.ClubArticlesCommentsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -31,9 +31,20 @@ public class ArticleCommentController {
 		return new ResponseEntity<>(savedComment, HttpStatus.CREATED);
 	}
 	
-	@GetMapping
-	public ResponseEntity<List<ArticleCommentDTO>> showAricleAllComment(@PathVariable Integer articleId) {
-		
-	}
+	 @PutMapping("/{commentId}")
+	    public ResponseEntity<ArticleCommentDTO> updateComment(@PathVariable Integer commentId, @RequestBody ArticleCommentDTO commentDTO) {
+	        ArticleCommentDTO updatedComment = commentService.updateCommentById(commentId, commentDTO);
+	        if (updatedComment != null) {
+	            return ResponseEntity.ok(updatedComment);
+	        } else {
+	            return ResponseEntity.notFound().build();
+	        }
+	    }
+
+	    @DeleteMapping("/{commentId}")
+	    public ResponseEntity<Void> deleteComment(@PathVariable Integer commentId) {
+	    	commentService.deleteCommentById(commentId);
+	        return ResponseEntity.noContent().build();
+	    }
 	
 }
